5e12360cd8c7678cc39bb6a7ce67e7259879ad99
Draku <vladimir.ibazeta@gmail.com>
2017-10-11 13:37:06 -0500

    Inicializar pesos con los clusters seleccionados.

--- sphere.py
+++ sphere.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import uniform
+
 from dataset import DataSet
 
 
@@ -7,17 +9,34 @@ class Sphere:
         self.cluster_units = cluster_units  # Cantidad de cluster units que vamos a tener
         self.epochs = epochs  # Épocas que el algoritmo correrá
         self.data_set = data_set  # Diccionario de Data Sets
+        self.weight = None
 
-        p10 = self._build_tdata_clusters(data_set.P10)
-        p20 = self._build_tdata_clusters(data_set.P20)
-        p30 = self._build_tdata_clusters(data_set.P30)
+        # p10, p20, p30 son tuplas.
+        # En las cuales el índice 0 indica el cluster F1 y el índice 1 el cluster F2
+        self.p10 = self._build_tdata_clusters(data_set.P10)
+        self.p20 = self._build_tdata_clusters(data_set.P20)
+        self.p30 = self._build_tdata_clusters(data_set.P30)
+
+        # Validar la forma de las matrices
+        assert self.p10[0].shape == (10, 10, 3) and self.p10[1].shape == (10, 10, 3)
+        assert self.p20[0].shape == (10, 10, 3) and self.p20[1].shape == (10, 10, 3)
+        assert self.p30[0].shape == (10, 10, 3) and self.p30[1].shape == (10, 10, 3)
+
+        # Inicializar arreglo de pesos
+        self._init_weight_array()
 
     def run(self):
         epoch = 0
 
         while epoch < self.epochs:
-            # ...
+            def _iterate_data_tuple(data: tuple):
+                counter = 0
+                for cluster in range(0, 2):  # Clusters F1 y F2
+                    for r in range(0, 10):  # Los cluster ya fueron separados en una tupla, los índices son [0,99]
+                        for c in range(0, 10):
+                            print(data[cluster][r][c])
 
+            _iterate_data_tuple(self.p10)
             epoch += 1
 
     def _build_tdata_clusters(self, data: list) -> tuple:
@@ -25,6 +44,32 @@ class Sphere:
         f2_cluster = self._get_cluster(data[0][100:], data, add_to_index=100)  # Los 100 últimos
 
         return np.array(f1_cluster), np.array(f2_cluster)
+
+    def _init_weight_array(self):
+        _w = []
+        for i in range(0, self.cluster_units):
+            _w.append(self._get_cluster_unit())
+
+        self.weight = np.array(_w)
+
+    @staticmethod
+    def _get_cluster_unit():
+        return np.array([uniform(0.9, 1.0), uniform(0.9, 1.0), uniform(0.9, 1.0)])
+
+    @staticmethod
+    def _get_radius(epoch: int) -> int:
+        if epoch == 0:
+            return 5
+        elif epoch == 19:
+            return 4
+        elif epoch == 39:
+            return 3
+        elif epoch == 59:
+            return 2
+        elif epoch == 79:
+            return 1
+        else:
+            return 0
 
     @staticmethod
     def _get_cluster(cluster: list, data: list, add_to_index: int = 0) -> list:
